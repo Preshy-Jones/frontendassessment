b917ef4d2826fe4ee75793d453ac1d7160e9eba3
Precious Adedibu <adedibuprecious@gmail.com>
2022-07-11 18:35:15 +0100

    error handling for apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,29 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  gql,
+  HttpLink,
+  from,
 } from "@apollo/client";
+import { RetryLink } from "@apollo/client/link/retry";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "https://rickandmortyapi.com/graphql" }),
+]);
 
 const client = new ApolloClient({
-  uri: "https://rickandmortyapi.com/graphql",
+  // uri: "https://rickandmortyapi.com/graphql",
   cache: new InMemoryCache(),
+  link: link,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
