ec4f7aecaca90fdc7d11657a210c21a7678ba3f7
Precious Adedibu <adedibuprecious@gmail.com>
2022-07-13 00:09:34 +0100

    faith

--- components/blog/Blog.tsx
+++ components/blog/Blog.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import moment from "moment";
 import { Post } from "../../types";
@@ -11,19 +11,36 @@ const Blog: React.FC<Props> = ({ post }) => {
     <Box>
       <Grid>
         <Image src={post.featuredImage.url} width="100%" height="300px" />
-        <Flex alignItems="center">
+        <Flex justify="space-between">
           <Text letterSpacing={2} py={2}>
             {moment(post.createdAt).format("MMMM DD, YYYY")}
           </Text>
-          <Flex>
-            <Image width="30px" height="30px" src={post.author.photo.url} />
+          <Flex alignItems="center">
+            <Image
+              width="30px"
+              height="30px"
+              src={post.author.photo.url}
+              borderRadius="50%"
+              mr="4"
+            />
             <Text letterSpacing={2} py={2}>
               {post.author.name}
             </Text>
           </Flex>
         </Flex>
-        <Flex justify="space-between" px={3} bg="#F2FEFF" py={2} my={3}>
-          <Text fontWeight="bold">Tech</Text>
+        <Flex
+          justify="space-between"
+          px={3}
+          bg="#F2FEFF"
+          py={2}
+          my={3}
+          borderRadius="md"
+          color="white"
+          backgroundColor="pink.600"
+        >
+          <Text fontWeight="bold">
+            {post.categories.map((category) => category.name).join(",")}
+          </Text>
           <Text>5min read</Text>
         </Flex>
         <Text fontWeight="extrabold" fontSize="xl">
@@ -33,6 +50,7 @@ const Blog: React.FC<Props> = ({ post }) => {
         <Text fontSize="sm" color="#B9B9B9" fontWeight="bold" py={2}>
           {post.excerpt}
         </Text>
+        <Button>Read more</Button>
       </Grid>
     </Box>
   );
