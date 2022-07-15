5b289fa0abeacd2cb4bb89c1a409cd02769d6fbe
Precious Adedibu <adedibuprecious@gmail.com>
2022-07-15 11:42:15 +0100

    push

--- utils/filterData.ts
+++ utils/filterData.ts
@@ -12,6 +12,14 @@ export const filterData = {
       name: "Artificial Intelligence",
       slug: "artificial-intelligence",
     },
+    {
+      name: "Web 3",
+      slug: "web3",
+    },
+    {
+      name: "Music",
+      slug: "music",
+    },
   ],
   title: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z",
   minute_read: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
